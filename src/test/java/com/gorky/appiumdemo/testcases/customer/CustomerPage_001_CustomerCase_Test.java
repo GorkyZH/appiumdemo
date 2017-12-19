14324e0e5242e007b16ad24a6cc8abb09470e031
gorky <1018063128@qq.com>
2017-12-19 15:04:50 +0800

    修改了我的客户测试用例类

--- src/test/java/com/gorky/appiumdemo/testcases/customer/CustomerPage_001_CustomerCase_Test.java
+++ src/test/java/com/gorky/appiumdemo/testcases/customer/CustomerPage_001_CustomerCase_Test.java
@@ -1,15 +1,14 @@
 package com.gorky.appiumdemo.testcases.customer;
 
-import java.io.IOException;
-import java.util.Map;
 
 import org.testng.annotations.Test;
 
 import com.gorky.appiumdemo.base.BasePrepare1;
 import com.gorky.appiumdemo.pages.HomePage;
-import com.gorky.appiumdemo.pages.MyguestPage;
+import com.gorky.appiumdemo.pages.TitlebarPage;
+import com.gorky.appiumdemo.pageshelper.CustomerPageHelper;
 import com.gorky.appiumdemo.pageshelper.HomePageHelper;
-import com.gorky.appiumdemo.pageshelper.MyguestPageHelper;
+import com.gorky.appiumdemo.pageshelper.TitlebarPageHelper;
 
 /**
  * @author GUO
@@ -17,41 +16,28 @@ import com.gorky.appiumdemo.pageshelper.MyguestPageHelper;
  */
 
 public class CustomerPage_001_CustomerCase_Test extends BasePrepare1{
-//	protected boolean containsElement = true;
-	/**
-	 * 搜索客户号码
-	 */
-	@Test(dataProvider ="testData")
-	public void customerDemo(Map<String,String> data) throws InterruptedException, IOException {
-		/*containsElement = appiumUtil.doesElementsExist(HomePage.HP_TEXTVIEW_GETCUSTOMER_COUNT);
-		if(containsElement) {
-			System.out.println("containsElement:"+containsElement);
-			HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXTVIEW_GETCUSTOMER_COUNT);
-			MyguestPageHelper.clickSearchImageview(appiumUtil, MyguestPage.MP_IMAGEVIEW_SEARCH);
-			MyguestPageHelper.clearKeyword(appiumUtil, MyguestPage.MP_EDITTEXT_SEARCH);
-			System.out.println("关键词："+data.get("keyword"));
-			MyguestPageHelper.inputKeyword(appiumUtil, MyguestPage.MP_EDITTEXT_SEARCH, data.get("keyword"));
-			MyguestPageHelper.pressSearchkeyword(appiumUtil,MyguestPage.MP_EDITTEXT_SEARCH,"adb shell ime set com.meizu.flyme.input/com.meizu.input.MzInputService");
-			containsElement = false;
-		}else {*/
-			MyguestPageHelper.clearKeyword(appiumUtil, MyguestPage.MP_EDITTEXT_SEARCH);
-			System.out.println("关键词："+data.get("keyword"));
-			MyguestPageHelper.inputKeyword(appiumUtil, MyguestPage.MP_EDITTEXT_SEARCH, data.get("keyword"));
-			MyguestPageHelper.pressSearchkeyword(appiumUtil,MyguestPage.MP_EDITTEXT_SEARCH,"adb shell ime set com.meizu.flyme.input/com.meizu.input.MzInputService");
-//		}
-		
+	protected String actrual_text,actrual_num;
+	protected String expected_num;
 	
-//		MyguestPageHelper.clickCancel(appiumUtil, MyguestPage.MP_TEXTVIEW_SEARCHCANCEL);
+	//判断我的客户页面标题与首页我的客户入口标题是否一致
+	@Test
+	public void customerCase1() {
+		actrual_text = TitlebarPageHelper.getText(appiumUtil, TitlebarPage.TP_TEXTVIEW_TITLE);
+		System.out.println("实际标题："+actrual_text);
+		CustomerPageHelper.isTextCorrect(appiumUtil,"我的客户", actrual_text);
+		
+		//返回首页
+		HomePageHelper.clickText(appiumUtil,HomePage.HP_TEXT_GETCUSTOMER);
 	}
-
+	
 	@Override
 	protected String getModuleName() {
-		return "customer";
+		return null;
 	}
 
 	@Override
 	protected String getCaseNum() {
-		return "001";
+		return null;
 	}
 	
 }
