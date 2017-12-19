27122a2ebeccebb036bd0b57a6f68d8ccfa57f0f
gorky <1018063128@qq.com>
2017-12-19 15:08:10 +0800

    修改了“从首页跳转到我的客户页面”的用例方法

--- src/test/java/com/gorky/appiumdemo/testcases/home/HomePage_001_HomeCase_Test.java
+++ src/test/java/com/gorky/appiumdemo/testcases/home/HomePage_001_HomeCase_Test.java
@@ -4,46 +4,26 @@ import org.testng.annotations.Test;
 
 import com.gorky.appiumdemo.base.BasePrepare1;
 import com.gorky.appiumdemo.pages.HomePage;
-import com.gorky.appiumdemo.pages.TitlebarPage;
 import com.gorky.appiumdemo.pageshelper.HomePageHelper;
 
 /**
  * @author GUO
- * @description 编写首页测试用例
+ * @description 跳转到我的客户页面
  */
 public class HomePage_001_HomeCase_Test extends BasePrepare1{
+	public static String str1,str2;
 	
 	@Test
 	public void homeDemo() {
-		System.out.println("homeclass");
-		System.out.println("点击我的客户数"+HomePage.HP_TEXTVIEW_GETCUSTOMER_COUNT);
-		
 		HomePageHelper.waitHomeUI(appiumUtil, elementTimeOut);
-	//	HomePageHelper.clickText(appiumUtil, HomePage.HP_IMAGEVIEW_MORE);
-		
-		/**
-		 * 点击我的客户数，进入我的客户页面
-		 */
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXTVIEW_GETCUSTOMER_COUNT);
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXT_GETCUSTOMER);
-		
-		
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXTVIEW_GETCUSTOMER_INCALLING);
-		HomePageHelper.clickText(appiumUtil, TitlebarPage.TP_IMAGEVIEWE_GOBACK);
-		
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXTVIEW_GETCUSTOMER_ROBGUEST);
-		HomePageHelper.clickText(appiumUtil, TitlebarPage.TP_IMAGEVIEWE_GOBACK);
-		
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXTVIEW_GETCUSTOMER_INCALLINGNUM);
-		HomePageHelper.clickText(appiumUtil, TitlebarPage.TP_IMAGEVIEWE_GOBACK);
-		
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXTVIEW_GETCUSTOMER_ROBGUESTNUM);
-		HomePageHelper.clickText(appiumUtil, TitlebarPage.TP_IMAGEVIEWE_GOBACK);
-		
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXT_MYCUSTOMER);
-		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXT_GETCUSTOMER);
 	}
-
+	
+	//点击我的客户数，进入我的客户页面
+	@Test
+	public void intoMyguest() {
+		HomePageHelper.clickText(appiumUtil, HomePage.HP_TEXTVIEW_GETCUSTOMER_COUNT);
+	}
+	
 	@Override
 	protected String getModuleName() {
 		return null;
