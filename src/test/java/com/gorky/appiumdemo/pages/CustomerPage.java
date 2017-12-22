0759d9325a2199706768e3aebcf1bdf6765721f3
gorky <1018063128@qq.com>
2017-12-22 15:24:38 +0800

    11

--- src/test/java/com/gorky/appiumdemo/pages/CustomerPage.java
+++ src/test/java/com/gorky/appiumdemo/pages/CustomerPage.java
@@ -29,27 +29,32 @@ public class CustomerPage {
 	public static final By CP_LISTVIEW_MYGUEST = By.id("cn.jufuns.laikebao:id/lv_my_customer_list");
 	
 	/**
+	 * 定位元素：列表中第一个item
+	 */
+	public static final By CP_ITEM_MYGUEST = By.xpath("//android.widget.ListView[1]//android.widget.LinearLayout[1]//android.widget.RelativeLayout[1]");
+	
+	/**
 	 * 定位元素：item中的客户姓名
 	 */
-	public static final By RCP_TEXTVIEW_CUSTOMERNAME = By.id("cn.jufuns.laikebao:id/item_customer_name");
+	public static final By CP_TEXTVIEW_CUSTOMERNAME = By.id("cn.jufuns.laikebao:id/item_customer_name");
 	
 	/**
 	 * 定位元素：item中的电话号码
 	 */
-	public static final By RCP_TEXTVIEW_PHONE = By.id("cn.jufuns.laikebao:id/item_customer_phone");
+	public static final By CP_TEXTVIEW_PHONE = By.id("cn.jufuns.laikebao:id/item_customer_phone");
 	
 	/**
 	 * 定位元素：item中的项目名称
 	 */
-	public static final By RCP_TEXTVIEW_PROJECT = By.id("cn.jufuns.laikebao:id/tv_item_customer_project_name");
+	public static final By CP_TEXTVIEW_PROJECT = By.id("cn.jufuns.laikebao:id/tv_item_customer_project_name");
 	
 	/**
 	 * 定位元素：item中的拨号
 	 */
-	public static final By RCP_IMAGEVIEW_CALL = By.id("cn.jufuns.laikebao:id/iv_phone_call");
+	public static final By CP_IMAGEVIEW_CALL = By.id("cn.jufuns.laikebao:id/iv_phone_call");
 	
 	/**
 	 * 定位元素：item中的来源渠道
 	 */
-	public static final By RCP_TEXTVIEW_CHANNEL = By.id("cn.jufuns.laikebao:id/tv_channel");
+	public static final By CP_TEXTVIEW_CHANNEL = By.id("cn.jufuns.laikebao:id/tv_channel");
 }
